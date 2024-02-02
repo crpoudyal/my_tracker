4cc0c2ec76b69373947b71c3727aaf6d00f6d6e0
crpoudyal <crpoudyal@gmail.com>
2024-02-02 21:35:38 +0545

    integrating appwrite with flutter app

---
+++ lib/appwrite/appwrite.dart
@@ -0,0 +1,8 @@
+import 'package:appwrite/appwrite.dart';
+
+class Appwrite {
+  Client client = Client();
+  Appwrite() {
+    client.setEndpoint('https://cloud.appwrite.io/v1').setProject('my-tracker');
+  }
+}
