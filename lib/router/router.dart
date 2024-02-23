80794eb55f8b1bb49fc9a47510fa1302a1420ec3
crpoudyal <crpoudyal@gmail.com>
2024-02-23 10:43:00 +0545

    fix package issue

--- lib/router/router.dart
+++ lib/router/router.dart
@@ -1,12 +1,10 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:my_tracker/view/home_page.dart';
 
-import 'package:riverpod/riverpod.dart';
-
-final routerProvider = Provider<GoRouter>((ref){
-
+final routerProvider = Provider<GoRouter>((ref) {
   return GoRouter(routes: [
-    GoRoute(name:HomePage.name ,path: '/',builder: (_,__)=>const HomePage())
+    GoRoute(
+        name: HomePage.name, path: '/', builder: (_, __) => const HomePage())
   ]);
-
 });
