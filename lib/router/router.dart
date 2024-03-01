08d8adb86794b92c143f43660528a56e6c167d48
crpoudyal <crpoudyal@gmail.com>
2024-03-01 08:38:15 +0545

    auth with appwrite

--- lib/router/router.dart
+++ lib/router/router.dart
@@ -1,7 +1,11 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:get_it/get_it.dart';
 import 'package:go_router/go_router.dart';
 import 'package:my_tracker/Home_page/home_page.dart';
+import 'package:my_tracker/appwrite/appwrite.dart';
 import 'package:my_tracker/features/signup_screen/src/signup_screen.dart';
 
 final routerProvider = Provider<GoRouter>((ref) {
@@ -15,8 +19,11 @@ final routerProvider = Provider<GoRouter>((ref) {
         path: '/signup',
         name: SignupScreen.name,
         builder: (_, __) {
-          return SignupScreen(onSignup: (name, email, password) {
+          return SignupScreen(onSignup: (name, email, password) async {
             debugPrint('$name - $email - $password');
+            final appwrite = GetIt.instance.get<Appwrite>();
+            final user = await appwrite.createAccount(name, email, password);
+            debugPrint(jsonEncode(user ?? '{}'));
           });
         })
   ]);
